ffeac328a6f039c615d58c9a7b60556cf4cb7019
Phyllip Cho <phyllip3@users.noreply.github.com>
2025-05-02 16:33:09 -0400

    clean up config

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,18 +1,10 @@
 /** @type {import('next').NextConfig} */
-
-process.env.NEXT_PUBLIC_API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "";
-process.env.NEXT_PUBLIC_PORT = process.env.PORT || 3000;
-
 const nextConfig = {
-  basePath: process.env.NEXT_PUBLIC_API_BASE_URL,
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: `${
-          process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:9000"
-        }/api/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:9000"}/api/:path*`,
       },
     ];
   },
